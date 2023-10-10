003a2967d75be1af72650e526505b51d15437547
Divya <divyashekhar4444@gmail.com>
2023-10-10 08:53:42 +0530

    Added Files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import UseState from "./Component/Hooks/useState";
+// import UseEffect from "./Component/Hooks/useEffect";
+import UseReducer from "./Component/Hooks/useReducer";
+// import "./Component/Basics/Style.css";
+
+const App = () => {
+  return (
+    <>
+      <UseReducer />
+      {/* <UseEffect />
+      <UseState /> */}
+    </>
+  );
+};
+
+export default App;
